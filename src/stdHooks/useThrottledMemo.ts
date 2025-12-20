a70483b4e962ab62af71a0bd958a1aee41603f47
over <franciscosebastianyjunior@hotmail.com>
2025-12-19 21:37:32 -0300

    Remove old comment in useThrottledMemo

--- src/stdHooks/useThrottledMemo.ts
+++ src/stdHooks/useThrottledMemo.ts
@@ -1,25 +1,6 @@
 import { useHookState } from '../topoRuntime'
 import { useThrottle } from './useThrottle'
 
-// OLD
-/**
- * Memoizes a value, updating it at most once every `seconds` seconds.
- * @param seconds The number of seconds between updates.
- * @param factory A factory function to produce the value.
- * @param initialValue An initial value to use until the first update.
- * @param identifier An optional identifier to differentiate between different throttles in the same hook call.
- * @returns The memoized value.
- *
- * @example
- * ```ts
- * // Updates a random value every 0.5 seconds.
- * // When 0.5 seconds have not yet passed, the previous value is returned instead, preventing UI flicker.
- * const value = useThrottledMemo(0.5, () => math.random(), 0)
- * // Draws at 60 FPS, but the value only updates every 0.5 seconds.
- * label.Text = `Value: ${value}`
- * ```
- */
-
 /**
  * Memoizes a value, updating it at most once every `seconds`.
  *
